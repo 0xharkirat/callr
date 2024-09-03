d21831929df85b398d2aee3256b0982c1d848a61
Harkirat Singh <sandhukirat23@gmail.com>
2024-09-03 18:55:21 +1000

    static qr

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CallR',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CallR'),
+      ),
+      body: Center(
+        child: QrImageView(
+          data: '0478953783',
+          version: QrVersions.auto,
+          size: 200.0,
+        ),
+      ),
+    );
+  }
+}
